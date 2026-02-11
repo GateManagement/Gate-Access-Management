b4b8fa33790c81261205791141532a00f0aa6212
jbenitez <jordybeni867@gmail.com>
2026-02-11 01:05:12 -0600

    Added basic classes for users and garages

---
+++ Gate-Access-Management/Models/Garage.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gate_Access_Management.Models;
+
+public class Garage
+{
+    [Key]
+    public int GarageId { get; set; }
+    
+    [Required]
+    public GarageSize GarageSizeId { get; set; }
+    
+    [Required]
+    public int GarageBuilding { get; set; }
+    
+    [Required]
+    public int UserId { get; set; }
+    
+    [Required]
+    [DataType(DataType.Currency)]
+    public decimal GarageCost { get; set; }
+    
+}
+
+public enum GarageSize
+{
+    Small = 1,
+    Medium = 2,
+    Large = 3
+}
---
+++ Gate-Access-Management/Models/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gate_Access_Management.Models;
+
+public class User
+{
+    [Key] public int UserId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [Phone]
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    [StringLength(100)] public string? Address { get; set; }
+
+    [StringLength(50)] public string? State { get; set; }
+
+    [StringLength(10)]
+    [Display(Name = "Zip Code")]
+    public string? ZipCode { get; set; }
+
+    [EmailAddress] [StringLength(100)] public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 4)]
+    public string UserPin { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    [Display(Name = "Created On")]
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+    [DataType(DataType.DateTime)]
+    [Display(Name = "Modified On")]
+    public DateTime? ModifiedOn { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [Display(Name = "Last User Entrance")]
+    public DateTime? LastUserEntrance { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [Display(Name = "Last User Payment")]
+    public DateTime? LastUserPayment { get; set; }
+
+    [Display(Name = "Is Disabled")] public bool IsDisabled { get; set; } = false;
+}
